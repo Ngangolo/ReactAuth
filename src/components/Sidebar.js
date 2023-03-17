bf3b27dcd42d46663243fb1755c63151d318aa2f
Ngangolo <konengangolo54@gmail.com>
2023-03-17 17:52:15 +0100

    admin expert commit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -37,14 +37,14 @@ function Sidebar() {
                 </a>
                 <div className="sidebar">
 
-                    <div className="user-panel mt-3 pb-3 mb-3 d-flex">
+                    {/* <div className="user-panel mt-3 pb-3 mb-3 d-flex">
                         <div className="image"> 
                             <img src={process.env.PUBLIC_URL + '/dist/img/user2-160x160.jpg'} className="img-circle elevation-2" alt="User Image" />
                         </div>
                         <div className="info">
                             <a href="#" className="d-block"> {localStorage.getItem('auth_user.name')}</a>
                         </div>
-                    </div>
+                    </div> */}
 
                     {/* <div className="form-inline">
                         <div className="input-group" data-widget="sidebar-search">
